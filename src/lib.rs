673f00bb33df12ea4fded130799598a466ee7d5d
Frank Panetta <frank@zuudo.com.au>
2020-05-05 23:55:06 +1000

    move app to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,44 @@
+mod utils;
+
+use seed::{*, prelude::*};
+
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+struct Model {
+  pub val: i32,
+}
+
+impl Default for Model {
+  fn default() -> Self {
+      Self {
+          val: 0,
+      }
+  }
+}
+
+#[derive(Clone)]
+enum Msg {
+  Increment,
+}
+
+fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+  match msg {
+      Msg::Increment => model.val += 1,
+  }
+}
+
+fn view(model: &Model) -> Node<Msg> {
+  button![
+      simple_ev(Ev::Click, Msg::Increment),
+      format!("Hello World × {}", model.val)
+  ]
+}
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    utils::set_panic_hook();
+    App::builder(update, view)
+        .build_and_start();
+}
